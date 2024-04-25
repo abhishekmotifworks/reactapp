a31d65b0b553556380ceeafd20e84dd8b4a7b7d2
Abhishek.Jirwankar <abhishek.jirwankar@boydgroup.com>
2024-04-25 17:07:17 +0530

    updated title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sample Application
+          Sample Application updated
         </p>
       </header>
     </div>
