a7d72ae5cfe247bb29e1a02f81a884fb7b4c7457
abhishekmotifworks <168066818+abhishekmotifworks@users.noreply.github.com>
2024-04-26 15:56:37 +0530

    updated apply command

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Sample Application updated !!!!!
-          Testing release. 007
+          Updated "apply" command.
         </p>
       </header>
     </div>
