1d95c78dc0f0d8b6f5f0d84439ce473b11d2267e
abhishekmotifworks <168066818+abhishekmotifworks@users.noreply.github.com>
2024-05-07 17:37:35 +0530

    testign argocd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Abhishek Jirwankar:) Testing
+          Hello Abhishek Jirwankar:) argocd
         </p>
       </header>
     </div>
