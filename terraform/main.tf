8668957da3516b2f981e2f7c1a276e3ec95ab55d
abhishekmotifworks <168066818+abhishekmotifworks@users.noreply.github.com>
2024-05-13 17:27:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,6 @@
 terraform {
 backend "azure" {
-    resource_group_name = "reactapp"
-    storage_account_name = "reactappstorageacc"
-    container_name = "reactappcontainer"
-    key = "terraform.tfstate"
+
   }
 }
 
